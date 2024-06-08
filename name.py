c03cdf7034f2a3315b4afa0a2117af86e2302c2d
Jenkibear <127636124+JenkinHe@users.noreply.github.com>
2024-06-08 17:43:58 +1000

    more sys commands and slices

--- name.py
+++ name.py
@@ -1,6 +1,11 @@
 import sys
 
-try:
-    print("Hello, my name is",sys.argv[1])
-except IndexError:
-    print("write name")
+if len(sys.argv)<2:
+    sys.exit("too few args")
+# elif len(sys.argv)>2:
+#     sys.exit("too many args")
+
+# print("Hello my name is",argv[1])
+
+for arg in sys.argv[1:]:
+    print("hello, my name is",arg)

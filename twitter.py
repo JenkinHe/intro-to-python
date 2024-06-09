56f4511d00776c1ae4cbece9c315d366a7bef0c0
Jenkibear <127636124+JenkinHe@users.noreply.github.com>
2024-06-09 14:20:02 +1000

    better allowances for regex

--- twitter.py
+++ twitter.py
@@ -6,5 +6,9 @@ url = input("URL: ").strip()
 # username= url.removeprefix("https://twitter.com/","")
 # print(f"Username: {username}")
 
-username=re.sub(r"^https?://twitter\.com/","",url)
-print(f"Username: {username}")
+if matches:=re.search(r"^https?://(?:www\.)?twitter\.com/(.+)$",url, re.IGNORECASE):
+    print(f"Username:",matches.group(1))
+
+# username=re.sub(r"^(https?://)?(www\.)?twitter\.com/","",url)
+# print(f"Username: {username}")
+

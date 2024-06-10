5f3f38cf16d29ddb5ef8a5732549de1785b9ecf7
Jenkibear <127636124+JenkinHe@users.noreply.github.com>
2024-06-10 14:35:32 +1000

    use list for mutability

--- student.py
+++ student.py
@@ -1,11 +1,13 @@
 def main():
     student=get_student()
+    if student[0] =="Padma":
+        student[1]="RavenClaw"
     print(f"{student[0]} from {student[1]}")
 
 def get_student():
     name =input("Name: ")
     house =input("House: ")
-    return (name,house)
+    return [name,house]
 
 
 def get_name():

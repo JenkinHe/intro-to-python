6254a0ce35924766c845c01dcf5665b61df3b94d
Jenkibear <127636124+JenkinHe@users.noreply.github.com>
2024-06-10 14:39:28 +1000

    dictionaries for better readability

--- student.py
+++ student.py
@@ -1,13 +1,14 @@
 def main():
     student=get_student()
-    if student[0] =="Padma":
-        student[1]="RavenClaw"
-    print(f"{student[0]} from {student[1]}")
+    if student["name"]=="Padma":
+        student["house"]="RavenClaw"
+    print(f"{student['name']} from {student['house']}")
 
 def get_student():
-    name =input("Name: ")
-    house =input("House: ")
-    return [name,house]
+
+    name=input("Name: ")
+    house=input("House: ")
+    return {"name":name,"house":house}
 
 
 def get_name():

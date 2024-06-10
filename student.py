a39e07e590c1fcbf163532118a7e06634cefb280
Jenkibear <127636124+JenkinHe@users.noreply.github.com>
2024-06-10 14:43:21 +1000

    create class and assign attributes

--- student.py
+++ student.py
@@ -1,14 +1,16 @@
+class Student:
+    ...
+
+
 def main():
     student=get_student()
-    if student["name"]=="Padma":
-        student["house"]="RavenClaw"
-    print(f"{student['name']} from {student['house']}")
+    print(f"{student.name} from {student.house}")
 
 def get_student():
-
-    name=input("Name: ")
-    house=input("House: ")
-    return {"name":name,"house":house}
+    student=Student()
+    student.name =input("Name: ")
+    student.house=input("House: ")
+    return student
 
 
 def get_name():

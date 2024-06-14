52604dc45722c6f2fc769045b8722367996af2a8
Jenkibear <127636124+JenkinHe@users.noreply.github.com>
2024-06-14 21:29:51 +1000

    get methods creates an instance of a class with a class method

--- student.py
+++ student.py
@@ -5,45 +5,19 @@ class Student:
     #public and private instances work on the honor system
     def __str__(self):
         return f"{self.name} from {self.house}"
-    
-    @property
-    def name(self):
-        return self._name
-    
-    @name.setter
-    def name(self,name):
-        if not name:
-            raise ValueError("Missing Name")
-        self._name=name
 
-    @property
-    def house(self):
-        return self._house
-    
-    @house.setter
-    def house(self,house):
-        if house not in ["G","H","R","S"]:
-            raise ValueError("Invalid house")
-        self._house=house
+    @classmethod
+    def get(cls):
+        name=input("Name:")
+        house =input("House: ")
+        return cls(name,house)
     
 
 def main():
-    student=get_student()
-
+    student=Student.get()
     print(student)
 
-def get_student():
 
-    name =input("Name: ")
-    house=input("House: ")
-    return Student(name,house)
-
-
-def get_name():
-    return input("Name: ")
-
-def get_house():
-    return input("House: ")
 
 if __name__ =="__main__":
     main()

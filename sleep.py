0f00f6ccfdad5d640fb0df966a47811746611609
Jenkibear <127636124+JenkinHe@users.noreply.github.com>
2024-06-15 20:47:16 +1000

    generator yield returns piece by piece large pieces of data

---
+++ sleep.py
@@ -0,0 +1,11 @@
+def main():
+    n= int(input("whats n?"))
+    for s in sheep(n):
+        print(s)
+
+def sheep(n):
+    for i in range(n):
+        yield "sheep"* i
+
+if __name__ =="__main__":
+    main()
